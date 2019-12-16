5c0052ac12a1c11089dc806dd9bf187322ba85f4
Pablo Fernandez <pfdzm@users.noreply.github.com>
2019-12-16 17:26:53 +0100

    define post route for new notes, save to db.json

--- server.js
+++ server.js
@@ -11,21 +11,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "/public/index.html"), err => {
+    if (err) {
+      console.log("Could not load index html, ", err);
+    }
+  });
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
+  res.sendFile(path.join(__dirname, "/public/notes.html"), err => {
+    if (err) {
+      console.log("Could not load note html, ", err);
+    }
+  });
 });
 
 app.get("/api/notes", (req, res) => {
-  // const notes = await fs.readFile('./db/db.json', "utf-8");
-  // res.send(JSON.parse(notes))
   res.sendFile(path.join(__dirname, "/db/db.json"), err => {
     if (err) {
       console.error("Could not read database file, ", err);
     }
   });
+});
+
+app.post("/api/notes", async (req, res) => {
+  console.log("req body, ", req.body);
+  const dbArr = JSON.parse(await fs.readFile(path.join(__dirname, "/db/db.json"), "utf-8"));
+  dbArr.push(req.body);
+  await fs.writeFile(
+    path.join(__dirname, "/db/db.json"),
+    JSON.stringify(dbArr)
+  );
+  res.sendStatus(200);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
 });
 
 app.listen(PORT, err => {
